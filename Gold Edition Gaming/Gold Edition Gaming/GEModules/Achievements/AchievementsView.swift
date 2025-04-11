de498dabc5b552ce74cd7eb6e519ec85ad7b3f02
Dias <dias.atudinov@gmail.com>
2025-04-11 19:40:46 +0500

    new files

---
+++ Gold Edition Gaming/Gold Edition Gaming/GEModules/Achievements/AchievementsView.swift
@@ -0,0 +1,18 @@
+//
+//  AchievementsView.swift
+//  Gold Edition Gaming
+//
+//  Created by Dias Atudinov on 11.04.2025.
+//
+
+import SwiftUI
+
+struct AchievementsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AchievementsView()
+}
