2a58810dc7ca30964002985f73a7657517e40993
Dias <dias.atudinov@gmail.com>
2025-04-11 18:38:21 +0500

    Settings View add

--- Gold Edition Gaming/Gold Edition Gaming/GEModules/Settings/SettingsViewModelDC.swift
+++ Gold Edition Gaming/Gold Edition Gaming/GEModules/Settings/SettingsViewModelDC.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
-class SettingsViewModelDC: ObservableObject {
+class SettingsViewModelGE: ObservableObject {
     @AppStorage("musicEnabled") var musicEnabled: Bool = true
+    @AppStorage("soundEnabled") var soundEnabled: Bool = true
 }
