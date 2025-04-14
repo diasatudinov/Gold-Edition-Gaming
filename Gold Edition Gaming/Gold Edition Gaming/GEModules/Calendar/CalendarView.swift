8147b7a0006f846e3747e4ce6702cf4b7b3216a5
Dias <dias.atudinov@gmail.com>
2025-04-14 19:54:47 +0500

    new files

---
+++ Gold Edition Gaming/Gold Edition Gaming/GEModules/Calendar/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  Gold Edition Gaming
+//
+//  Created by Dias Atudinov on 14.04.2025.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
